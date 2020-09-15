ddc1d322c38c7e25f1aef15ccac9cd4dd037fc8a
Quinn <quinnvoker@gmail.com>
2020-09-15 00:00:32 +0000

    separated error messages into individual lines

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -14,4 +14,7 @@ print "Name of store to add\n> "
 store_name = gets.chomp
 new_store = Store.create(name: store_name)
 
-puts new_store.errors.messages
+new_store.errors.messages.each do |error|
+  puts ":#{error[0]}\n"
+  error[1].each { |message| puts "  #{message}" }
+end
